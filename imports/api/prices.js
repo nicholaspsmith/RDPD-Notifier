10daffb3765ccdf7e3dfd2a7c5c95406ba12b54b
Nicholas Smith <Nicholas Smith>
2017-03-02 22:12:00 -0500

    remove amazon product scrapes because amazon is blocking bots

--- imports/api/prices.js
+++ imports/api/prices.js
@@ -8,21 +8,27 @@ const nick = '+19158619472'
 export const Prices = new Mongo.Collection('prices')
 
 Meteor.methods({
-	'newPrice'(priceFromScraper, name, message) {
-		let price = parseInt(priceFromScraper.replace('$', '').replace(',',''))
-		const existingPrices = Prices.find({ name }, {sort: {date: -1} }).fetch()
-		if (existingPrices.length === 0) {
-			Prices.insert({ name, price, date: new Date() })
-			sendMessage(`Added ${name} to price tracker`)
-		} else {
-			let oldPrice = existingPrices[0].price
-
-			if (price != oldPrice) {
-				Prices.insert({ name, price, date: new Date() })
-				console.log(message)
+	'newPrice'(priceFromScraper, product) {
+		if (typeof priceFromScraper === 'undefined') {
+			console.log(`${product.name} price is undefined :(`)
+		} else {		
+			let price = parseInt(priceFromScraper.replace('$', '').replace(',',''))
+			const existingPrices = Prices.find({ name: product.name }, {sort: {date: -1} }).fetch()
+			if (existingPrices.length === 0) {
+				Prices.insert({ name: product.name, price, date: new Date() })
+				let message = `Added ${product.name} to price tracker`
 				sendMessage(message, nick)
 			} else {
-				console.log('Price is same ' + new Date())
+				let oldPrice = existingPrices[0].price
+
+				if (price != oldPrice) {
+					Prices.insert({ name: product.name, price, date: new Date() })
+			    let message = `The price of ${product.name} has changed. It is now $${price}`
+					console.log(message)
+					sendMessage(message, nick)
+				} else {
+					console.log(`Price of ${product.name} is ${price}`)
+				}
 			}
 		}
 	}
--- server/main.js
+++ server/main.js
@@ -6,48 +6,45 @@ import { Prices } from '../imports/api/prices'
 
 const rdpdSeminar = {
   name: 'Rich Dad Poor Dad 3 Day Real Estate Seminar',
-  url: 'https://richdadeducation.com/3DayTraining/RealEstate/Register'
+  url: 'https://richdadeducation.com/3DayTraining/RealEstate/Register',
+  divId: '#divAmount'
 }
 
 const surfacePro4 = {
   name: 'Surface Pro 4 i5, 8GB RAM, 256GB',
-  url: 'https://www.amazon.com/gp/product/B01606IDL0/ref=ox_sc_sfl_title_2?ie=UTF8&psc=1&smid=A1BCG1Y536QVQG',
+  url: 'https://www.amazon.com/dp/B01606IDL0/ref=twister_B0169A7O62?_encoding=UTF8&psc=1',
+  divId: '#priceblock_ourprice'
+}
+
+const surfacePro4i7 = {
+  name: 'Surface Pro 4 i7, 8GB RAM, 256GB',
+  url: 'https://www.amazon.com/dp/B01605ZRBK/ref=twister_B0169A7O62?_encoding=UTF8&th=1',
+  divId: '#priceblock_ourprice'
+}
+
+function scrapeSites() {
+  // Rich dad poor dad 3 day seminar
+  scraperjs.StaticScraper.create(rdpdSeminar.url)
+  .scrape(function($) {
+    return $("#divAmount").map(function() {
+      return $(this).text().match(/\$.*/);
+    }).get();
+  })
+  .then(Meteor.bindEnvironment(function(price) {
+    if (typeof price === 'object') price = price[0]
+    let message = `The price of the Rich Dad Poor Dad 3 Day Seminar has changed. It is now $${price}`
+    message += `\n${rdpdSeminar.url}`
+    Meteor.call('newPrice', price, rdpdSeminar)
+  }))
 }
 
 SyncedCron.add({
-  name: 'Get the date!',
+  name: 'Get prices',
   schedule(parser) {
     return parser.text('every 1 hours')
   },
   job() {
-  // Rich dad poor dad 3 day seminar
-	scraperjs.StaticScraper.create(rdpdSeminar.url)
-    .scrape(function($) {
-      return $("#divAmount").map(function() {
-        return $(this).text().match(/\$.*/);
-      }).get();
-    })
-    .then(Meteor.bindEnvironment(function(price) {
-      if (typeof price === 'object') price = price[0]
-      let message = `The price of the Rich Dad Poor Dad 3 Day Seminar has changed. It is now $${price}`
-      message += `\n${rdpdSeminar.url}`
-      Meteor.call('newPrice', price, rdpdSeminar.name, message)
-    }))
-
-    // surface pro 4
-    scraperjs.StaticScraper.create(surfacePro4.url)
-    .scrape(function($) {
-      return $("#priceblock_ourprice").map(function() {
-        return $(this).text().match(/\$.*/);
-      }).get();
-    })
-    .then(Meteor.bindEnvironment(function(price) {
-      if (typeof price === 'object') price = price[0]
-      let message = `The price of ${surfacePro4.name} has changed. It is now ${price}`
-      Meteor.call('newPrice', price, surfacePro4.name, message)
-    }))
-
-    
+    scrapeSites()
   }
 })
 
@@ -55,5 +52,6 @@ Meteor.startup(function () {
   process.env.TWILIO_ACCOUNT_SID = 'AC39590e0b047a8d8f43334d4a52b2a388';
   process.env.TWILIO_AUTH_TOKEN = '142baeef915c558c1d54d9b42dd94071';
 
-	SyncedCron.start();
+  scrapeSites();
+  SyncedCron.start();
 })
